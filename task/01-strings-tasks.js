a8ba24b84a0021775d99cda480d9edc9b6a787a2
Dzmitry Dzenisiuk <d.dzenisiuk@gmail.com>
2020-05-20 21:46:24 +0300

    string task edit

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -198,7 +198,9 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-  return '┌' + '─'.repeat(width - 2) + '┐' + '\n' + ('│' + ' '.repeat(width - 2) + '│' + '\n').repeat(height - 2) + '└' + '─'.repeat(width - 2) + '┘' + '\n';
+  return '┌' + '─'.repeat(width - 2) + '┐' + '\n' +
+    +('│' + ' '.repeat(width - 2) + '│' + '\n').repeat(height - 2) +
+    +'└' + '─'.repeat(width - 2) + '┘' + '\n';
 }
 
 
